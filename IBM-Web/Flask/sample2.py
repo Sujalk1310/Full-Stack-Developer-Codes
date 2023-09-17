755435349680aba0cb5c2d68aa3747da275a9b3c
Sujal Kulshrestha <92009370+Sujalk1310@users.noreply.github.com>
2023-09-17 21:05:43 +0530

    go go go

---
+++ IBM-Web/Flask/sample2.py
@@ -0,0 +1,15 @@
+'''
+Code for adding two numbers
+'''
+
+def add(number1, number2):
+    '''
+    Function of add
+    '''
+    return number1 + number2
+
+NUM_1 = 4
+NUM_2 = 5
+TOTAL = add(NUM_1, NUM_2)
+
+print(f"The sum of {NUM_1} and {NUM_2} is {TOTAL}")
